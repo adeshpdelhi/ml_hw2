652c7c7530bb209f09e3304e09ce060adea2a9f3
Adesh Pandey <pandeyadeshdelhi@gmail.com>
2016-09-15 22:12:53 +0530

    modified a bit for upload

--- code/main.m
+++ code/main.m
@@ -1,42 +1,42 @@
 adapt_data();
 
-% disp('lin dataset');
-% original_dataset = load('lin.txt');
-% X = original_dataset(:,1:end-1);
-% y = original_dataset (:,end);
-% [X,y] = normalize(X,y);
-% execute('Lin',X,y,0, 0,true, true, 0.0005,false);
-% execute('Lin',X,y,0, 1,true, true, 0.0005,false);
+disp('lin dataset');
+original_dataset = load('lin.txt');
+X = original_dataset(:,1:end-1);
+y = original_dataset (:,end);
+[X,y] = normalize(X,y);
+execute('Lin',X,y,0, 0,true, true, 0.0005,false); %linear regression
+execute('Lin',X,y,0, 1,true, true, 0.0005,false); %polynomial
 
-% execute('Lin', X,y,1, 1, false, false, 0.0005,false); % optimal is 1
-
-
-% disp('Sph dataset');
-% original_dataset = load('sph.txt');
-% X = original_dataset(:,1:end-1);
-% y = original_dataset (:,end);
-% [X,y] = normalize(X,y);
-% execute('Sph',X,y,0, 0,true, true, 0.005,false);
-% execute('Sph',X,y,0, 1,true, true, 0.005,false);
-
-% execute('Sph', X,y,0.5, 1, true, true, 0.005,false); % optimal 0.5
-
-% disp('Iris dataset');
-% original_dataset = load('iris2.data');
-% X = original_dataset(:,1:end-1);
-% y = original_dataset (:,end);
-% [X,y] = normalize(X,y);
-% execute('Iris',X,y, 0.2, 0,false, true, 0.005,false);
-% execute('Iris',X,y, 0.2, 0,false, true, 0.005,true);
+execute('Lin', X,y,0.1, 0, true, true, 0.0005,false); % ridge regression .... optimal del= 0.1
 
 
-% disp('Seeds dataset');
-% original_dataset = load('seeds_dataset.txt');
-% X = original_dataset(:,1:end-1);
-% y = original_dataset (:,end);
-% [X,y] = normalize(X,y);
-% execute('Seeds',X,y, 0.5, 0,false, true, 0.001,false);
-% execute('Seeds',X,y, 0.5, 0,false, true, 0.001,true);
+disp('Sph dataset');
+original_dataset = load('sph.txt');
+X = original_dataset(:,1:end-1);
+y = original_dataset (:,end);
+[X,y] = normalize(X,y);
+execute('Sph',X,y,0, 0,true, true, 0.005,false); %linear regression
+execute('Sph',X,y,0, 1,true, true, 0.005,false); %polynomial
+
+execute('Sph', X,y,0.5, 0, true, true, 0.005,false); % ridge regression ..... optimal del= 0.5
+
+disp('Iris dataset');
+original_dataset = load('iris2.data');
+X = original_dataset(:,1:end-1);
+y = original_dataset (:,end);
+[X,y] = normalize(X,y);
+execute('Iris',X,y, 0.2, 0,false, true, 0.005,false); %ridge regression
+execute('Iris',X,y, 0.2, 0,false, true, 0.005,true); %part e
+
+
+disp('Seeds dataset');
+original_dataset = load('seeds_dataset.txt');
+X = original_dataset(:,1:end-1);
+y = original_dataset (:,end);
+[X,y] = normalize(X,y);
+execute('Seeds',X,y, 0.5, 0,false, true, 0.001,false); %ridge regression
+execute('Seeds',X,y, 0.5, 0,false, true, 0.001,true); %part e
 
 
 
@@ -48,8 +48,8 @@ original_dataset = original_dataset(:,[ 1: a - 1,b , a + 1: b - 1,a,b+1 :end]);
 X = original_dataset(:,1:end-1);
 y = original_dataset (:,end);
 [X,y] = normalize(X,y);
-% execute('Airquality',X,y,0.2, 0,false, true, 0.00001,false);
-execute('Airquality',X,y,0.2, 0,false, true, 0.00001,true);
+execute('Airquality',X,y,0.2, 0,false, true, 0.00001,false); %ridge regression
+execute('Airquality',X,y,0.2, 0,false, true, 0.00001,true); %part e
 
 
 
--- readme.txt
+++ readme.txt
@@ -1,3 +1,4 @@
 The input X to linear_regression doesn't contain the first column as all 1's.
 I am scaling the features of the data before passing it to linear_regresion. The function should work even without any scaling.
 I am scaling to keep alpha sensibly small otherwise it used to blow up rapidly.
+Linear regression prototype: function [final_parameters min_cost] = linear_regression(X, phi, max_itr, del, alpha)
